a4c00c0773c1e67d99bff7f60b95a9fb8272fc99
Meghana <meghanagajanana14@gmail.com>
2022-09-14 20:49:10 +0200

    added files

---
+++ src/Recipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './recipemodule.css'
+const Recipe = ({title,calories,image,ingredients}) => {
+    return(
+        <div>
+            <div className={style.recipe}>
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>{calories}</p>
+            <img src={image} alt=""/>
+            </div>
+        </div>
+    )
+}
+export default Recipe;
